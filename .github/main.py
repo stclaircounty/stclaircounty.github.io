17d2600dc4fc2502a2a22cc0e5fa429c2e5ed01e
Steven Crothers <scrothers@users.noreply.github.com>
2025-07-21 09:56:46 -0400

    Fix pathing for main.py

--- .github/main.py
+++ .github/main.py
@@ -3,5 +3,5 @@ import yaml
 def define_env(env):
     @env.macro
     def get_mesh_channels():
-        with open("../data/meshtastic.yml") as f:
+        with open("data/meshtastic.yml") as f:
             return yaml.safe_load(f)
